1da1c5fb30fd6719b2abe860955fb270af09a28c
Excellence <352481@mail.ru>
2024-07-09 17:01:17 +0300

    Добавил приложение base

--- shop/settings.py
+++ shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
     'main.apps.MainConfig',
     'basket.apps.BasketConfig',
     'catalog.apps.CatalogConfig',
--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+import basket.views
+import catalog.views
+import contacts.views
+import main.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
-    path('basket/', include('basket.urls')),
-    path('contacts/', include('contacts.urls')),
-    path('catalog/', include('catalog.urls')),
+    path('main/', main.views.main),
+    path('basket/', basket.views.main),
+    path('contacts/', contacts.views.main),
+    path('catalog/', catalog.views.main),
 ]
