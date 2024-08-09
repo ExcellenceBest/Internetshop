8ed62f03717b3639f13979e46ab8f4339a0d2a40
Excellence <352481@mail.ru>
2024-08-09 17:13:32 +0300

    коммит с работы

--- basket/database.py
+++ basket/database.py
@@ -69,8 +69,7 @@ class PGProductsManager(DBManager):
 
                 params = (product.title, )
                 query = """SELECT * 
-                           FROM shampoo
-                           WHERE sh_title LIKE %s"""
+                           FROM item"""
                 cursor.execute(query, params)
                 data = cursor.fetchall()
 
--- basket/views.py
+++ basket/views.py
@@ -13,11 +13,12 @@ def main(request: HttpRequest):
                                         password='week0497')
 
         cursor = connect.cursor()
-        query = """ SELECT * FROM shampoo """
+        query = """ SELECT * FROM item """
         cursor.execute(query)
         container = ProductsContainer()
         container.create_list_product(cursor.fetchall())
         data = container.get_list_product()
+        print(data[0])
         count = len(data) if data is not None else 0
         cursor.close()
     except ValueError as e:
@@ -74,30 +75,32 @@ def in_basket(request: HttpRequest):
                                       user='postgres',
                                       password='week0497')
 
-    cursor = connect.cursor()
     product_id = request.POST.get('product_id', '')
-    query = """ UPDATE shampoo
-                SET quantity = quantity - 1
-                WHERE shampoo_id = shampoo_id """
-    cursor.execute(query, product_id)
-    cursor.close()
+
+    # cursor = connect.cursor()
+    # query = """ UPDATE shampoo
+    #             SET quantity = quantity - 1
+    #             WHERE shampoo_id = shampoo_id """
+    # cursor.execute(query, product_id)
+    # cursor.close()
+    param = product_id
     cursor = connect.cursor()
     query = """ SELECT *
-                    FROM shampoo
-                    WHERE shampoo_id = shampoo_id """
-    cursor.execute(query, product_id)
+                    FROM item
+                    WHERE item_id = %s """
+    cursor.execute(query, param)
     data = cursor.fetchall()
-    params = (data[0][1], data[0][2], data[0][3], data[0][4], data[0][5], 1)
+    print(product_id)
+    params = (data[0][1], data[0][2], data[0][3], data[0][4], data[0][5])
     container = ProductsContainer()
     container.create_list_product(data)
-
-
     cursor.close()
-    cursor = connect.cursor()
-    query = """ INSERT INTO basket(sh_title, sh_view, manufacturer, description, price, quantity)
-                                 VALUES (%s, %s, %s, %s, %s, %s)"""
-    print(data)
-    cursor.execute(query, params)
-    cursor.close()
+
+    # cursor = connect.cursor()
+    # query = """ INSERT INTO basket(sh_title, sh_view, manufacturer, description, price)
+    #                              VALUES (%s, %s, %s, %s, %s)"""
+    # print(data)
+    # cursor.execute(query, params)
+    # cursor.close()
     connect.commit()
     return render(request, 'end_pay.html')
