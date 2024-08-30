d617f62d580d119a61290d64b6573954fbd32b89
Peps@ <352481@mail.ru>
2024-08-30 14:55:59 +0300

    Преподготовка

--- about/views.py
+++ about/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 
 def main(request: HttpRequest):
-    return render(request, 'about2.html')
+    return render(request, 'about.html')
 
 def redirect(request: HttpRequest):
     return render(request, '404.html')
